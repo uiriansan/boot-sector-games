1eb2aa9635187b995fea8a2e909e5ec6b4ac49ce
uiriansan <uiriansan@gmail.com>
2025-07-03 15:54:04 -0300

    Fix background draw_loop

--- test.asm
+++ test.asm
@@ -6,36 +6,24 @@ main:
     mov al, 13h
     int 10h
 
-    push 0
-    push 0
-
-draw_pixel_loop:
+draw_loop:
     mov ah, 0ch
     mov al, 4
     mov bh, 1
-    pop cx
-    inc cx
-    cmp cx, 319
-    je increment_row
-
-    pop dx
     int 10h
 
-    push dx
-    push cx
-    jmp draw_pixel_loop
+    inc cx
+    cmp cx, 319
+    jne draw_loop
+
+    mov cx, 0
+    inc dx
+    cmp dx, 200
+    jne draw_loop
 
     hlt
 halt:
     jmp halt
-
-increment_row:
-    pop dx
-    inc dx
-    push dx
-    push 0
-    jmp draw_pixel_loop
-
 
 times 510-($-$$) db 0
 dw 0xaa55
